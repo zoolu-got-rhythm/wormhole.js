e03863c8db3c96fe72a85e7f9f5452221aee078a
Christopher P.K Morris <chris_pkm@hotmail.co.uk>
2015-09-08 23:14:28 +0100

    experiementation 1

---
+++ app.js
@@ -0,0 +1,15 @@
+// call a new sequence/instance every couple secs. which then expands off the screen.
+//sT(this).checkDimensions();
+
+// instance 1 of wormhole library
+var inst1 = sT(this);
+
+// method chaining
+
+
+function beam(){
+  this.setInterval(beam, 1000); // should this be at the end of the func? recursion
+  inst1.checkDimensions().sequence();
+}
+
+beam();
